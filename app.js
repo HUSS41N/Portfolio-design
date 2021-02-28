3af3f5539515ed4aefcb104bbc1eda01da9e6efc
MD Hussain <>
2021-02-28 16:32:49 +0530

    main

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ nav_show = () => {
     })
 }
 nav_show()
+
+// Scroll animation
+scroll_animation = () => {
+    window.addEventListener('scroll',()=>{
+        let main_head = document.querySelector('.main-head')
+        main_head.classList.toggle('main-head-color-change',window.scrollY > 0)
+    })
+}
+scroll_animation()
